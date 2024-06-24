acc64528aaeec38cfd141de6b91ce2a2d69618a0
Kalyan720 <119833878+Kalyan720@users.noreply.github.com>
2024-06-25 00:54:34 +0530

    Add files via upload
    Zero commit

---
+++ app/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+import Hero from "@/components/Hero";
+import Grid from "@/components/Grid";
+import Footer from "@/components/Footer";
+import Approach from "@/components/Approach";
+import Experience from "@/components/Experience";
+import RecentProjects from "@/components/RecentProjects";
+import Navibar from "@/components/ui/Navbar";
+
+
+const Home = () => {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <Navibar />
+        <Hero />
+        <Grid />
+        <RecentProjects />
+        <Experience />
+        <Approach />
+        <Footer />
+      </div>
+    </main>
+  );
+};
+
+export default Home;
